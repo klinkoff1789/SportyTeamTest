13b40add3e9d437020c24105c30b19fc7ec35bf3
MLuhovyi <m.luhovyi@gmail.com>
2026-01-18 11:50:11 +0100

    014 added templates for base_page.py & template_mobile_page.py

---
+++ pages/template_base_page.py
@@ -0,0 +1,32 @@
+from playwright.sync_api import Page
+
+
+class BasePage:
+    default_timeout = 10000
+
+    def __init__(self, page: Page):
+        self.page = page
+
+#TODO for further implementation if necessary
+
+    # def wait_element_visible(self, selector: str) -> None:
+    #     try:
+    #         locator = self.page.locator(selector)
+    #         locator.wait_for(state="visible", timeout=self.default_timeout)
+    #     except Exception as e:
+    #         raise Exception(f"Error while waiting for element '{selector}' to become visible: {str(e)}")
+    #
+    # def click_on_element(self, selector: str) -> None:
+    #     try:
+    #         self.wait_element_visible(selector)
+    #         self.page.locator(selector).click()
+    #     except Exception as e:
+    #         raise Exception(f"Error while clicking on the element {str(e)}")
+    #
+    # def get_text(self, selector: str) -> str:
+    #     try:
+    #         self.wait_element_visible(selector)
+    #         return self.page.locator(selector).inner_text()
+    #     except Exception as e:
+    #         raise Exception(f"Error while getting text from the element: {str(e)}")
+    #
---
+++ pages/template_twitch_mobile_page.py
@@ -0,0 +1,14 @@
+# import re
+#
+# from playwright.sync_api import Page, expect
+#
+# from data.urls import TwitchURL
+# from locators.media_locators import COOKIES_ACCEPT_BUTTON, MOB_SEARCH_ICON, MOB_SEARCH_FIELD
+from pages.template_base_page import BasePage
+
+#TODO has to be implemented later
+class TwitchMobilePage(BasePage):
+    def __init__(self, base_page):
+        super().__init__(base_page)
+
+#TODO implement some methods if required
